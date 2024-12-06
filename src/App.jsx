336b8ac8cfb018d487e800ba00f4043023d8993d
iRyan <irenesryan@gmail.com>
2024-12-06 14:46:33 +0100

    Call keys from env file and update array content to align with the app purpose.

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import { SearchBox, initializeIcons } from '@fluentui/react';
 
 initializeIcons();
 
+// Access the environment variables
+const projectId = import.meta.env.VITE_REACT_APP_PROJECT_ID;
+const agentId = import.meta.env.VITE_REACT_APP_AGENT_ID;
+
 function App() {
+
   const [submittedQuery, setSubmittedQuery] = useState('');
   const [matchFound, setMatchFound] = useState([]);
   const [hasSearched, setHasSearched] = useState(false);
   const searchRef = useRef(null);
-  
+
   // Custom styles for the SearchBox
   const searchBoxStyles = {
     root: {
@@ -28,99 +33,124 @@ function App() {
           borderLeft: '0px solid transparent',
         },
     },
-    iconContainer: {    
+    iconContainer: {
       color: 'grey',
-      display:'none'
-    }
+      display: 'none',
+    },
   };
-  
-  // Array of country names
-  const countries = [
-    "United States", "Canada", "Mexico", "Brazil", "Argentina",
-    "France", "Germany", "Spain", "Sweden", "Italy", "United Kingdom",
-    "China", "Japan", "South Korea", "India", "Australia",
-    "New Zealand", "South Africa", "Egypt", "Nigeria", "Kenya"
+
+  const gitcommands = [
+    'git', 'git clone', 'git checkout', 'git commit',
+    'git fetch', 'git push', 'git pull', 'git merge',
+    'git branch', 'git push origin master',
   ];
 
-  // function - 1
+  // Handle input changes
   const handleChange = (_, newValue) => {
     setSubmittedQuery(newValue); // Update the state on each keystroke to keep the input responsive
     if (!newValue) {
       setHasSearched(false);
-      setMatchFound(''); // Clear the matchFound state when the input is empty
+      setMatchFound([]); // Clear the matchFound state
     }
   };
 
-  // function - 2
+  // Handle search submit
   const handleSearchSubmit = (value) => {
     setHasSearched(true); // Set the hasSearched state to true when the
     
-    // Check if the typed country name exists in the array
-    const matchFound = countries.find((country) =>
-      country.toLowerCase() === value.toLowerCase()
+    // Check if the typed git command exists in the array
+    const matchFound = gitcommands.find((gitcmd) =>
+      gitcmd.toLowerCase() === value.toLowerCase()
     );
     if (matchFound) {
-      setMatchFound(`Yes, match found: ${matchFound}`); // Update the state with the matched country name
+      setMatchFound(
+        <span>
+          Yes, match found: <b style={{ fontWeight: 'bold' }}>{matchFound}</b> is a git command
+        </span>,
+      );
     } else {
       setMatchFound('No match found'); // Clear the state if no match is found
     }
-    
-    setSubmittedQuery(value); //store the value only on enter key
-  }
 
-  // function - 3
+    setSubmittedQuery(value);
+  };
+
+  // Clear the search input
   const handleClear = () => {
     setSubmittedQuery('');
-    setMatchFound('');
+    setMatchFound([]);
     setHasSearched(false);
 
     if (searchRef.current) {
       searchRef.current.focus();
     }
-  }  
+  };
 
   return (
-    <div style={{background:'#f5f5f5'}}>
-      <div style={{marginBottom:30, background:'grey'}}>
-        <h1 style={{color:'black', padding:'10px'}}>Fluent UI SearchBox Test</h1>
+    <div style={{ background: '#f5f5f5', paddingBottom: '30px', marginTop: '50px' }}>
+      <div style={{ marginBottom: 20, background: '#f5f5f5', color: 'grey', padding: '25px' }}>
+        <h2>AI Chatbot with custom knowledge base</h2>
       </div>
 
-      <div style={{display:'flex', alignItems:'center', justifyContent:'center', background:'#b0a196', padding:'5px'}}>
-        <h5 style={{padding:'5px', paddingRight:'10px', fontSize:'15px'}}>Search Box</h5>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          background: '#b0a196',
+          padding: '5px',
+          paddingBottom: '20px',
+          flexDirection: 'column',
+        }}
+      >
+        <div><h3 style={{ color: 'black', padding: '10px' }}>Search Git Command</h3></div>
         <SearchBox
           componentRef={searchRef}
-          placeholder='Search here...'
+          placeholder="Search here..."
           onSearch={handleSearchSubmit} //trigger search on enter key
           onChange={handleChange}
           value={submittedQuery}
           onClear={handleClear}
-          styles={{...searchBoxStyles}}
+          styles={searchBoxStyles}
         />
       </div>
+
+      <br />
+
+      {hasSearched && matchFound && (
+        <div style={{
+          display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#ccb6c9',
+        }}
+        >
+          <h5 style={{ padding: '14px' }}>Search Value</h5>
+          <p style={{
+            fontSize: '14px',
+            border: '1px solid #0078d4',
+            borderRadius: '5px',
+            padding: '7px',
+          }}
+          >
+            {matchFound}
+          </p>
+        </div>
+      )}
+
       <div>
-      <df-messenger
+        <df-messenger
           className="df-messenger"
-          project-id="testpro-441710"
-          agent-id="fe947209-7963-425f-b1be-7d1aa2ddb7f7"
+          project-id={projectId}
+          agent-id={agentId}
           language-code="en"
           max-query-length="-1"
-          style={{bottom: '20px', right: '40px', position: 'fixed' }}
+          style={{
+            bottom: '20px', right: '40px', position: 'fixed',
+          }}
         >
-          <df-messenger-chat-bubble chat-title="test pro agent"></df-messenger-chat-bubble>
+          <df-messenger-chat-bubble chat-title="test pro agent" />
         </df-messenger>
       </div>
-      
-      <br />
-      {/* Conditionally render the matched country result in a styled text box */}
-      {
-        hasSearched && matchFound && (
-          <div style={{display:'flex', alignItems:'center', justifyContent:'center', background:'#ccb6c9'}}>
-            <h5 style={{padding:'14px'}}>Search Value</h5>
-            <p style={{fontSize:'14px', border: '1px solid #0078d4', borderRadius: '5px', padding: '7px'}}>{matchFound}</p>
-          </div>
-        )
-      }
     </div>
   );
 }
+
 export default App;
